0bb105c7fb0110dd1bd2c69977fb243d5a0c543c
omaremam1 <oemam6062379@gmail.com>
2023-08-18 22:46:29 +0300

    aaaa

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-let port = process.env.PORT || 5000;
-
+const express = require("express");
 const admin = require("firebase-admin");
 const credentials = require("./hemaya-860b8-firebase-adminsdk-jv1xa-ee5d71199f.json");
-const firebase = require("firebase/app");
 require("firebase/firestore");
+const bodyParser = require("body-parser");
 
-
-
- admin.initializeApp({
+admin.initializeApp({
   credential: admin.credential.cert(credentials)
 });
 
-const db = admin.firestore()
+const db = admin.firestore();
+const app = express();
+
+app.use(bodyParser.json());
 
 let IO = require("socket.io")(port, {
   cors: {
@@ -32,76 +32,7 @@ IO.on("connection", (socket) => {
   console.log(socket.user, "Connected");
   socket.join(socket.user);
 
-  socket.on("makeCall", (data) => {
-    let calleeId = data.calleeId;
-    let sdpOffer = data.sdpOffer;
-    let name = data.name;
-    let lat = data.lat;
-    let long = data.long;
-    let userId = data.userId
-
-    db.collection("sessions").add({
-      userId: userId,
-      isAnswered: false,
-      timestamp: new Date()
-    });
-    
-
-    socket.to(calleeId).emit("newCall", {
-      callerId: socket.user,
-      sdpOffer: sdpOffer,
-      name: name,
-      lat: lat,
-      long: long
-    });
-  });
-
-  socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
-    let userId = data.userId
-
-    console.log("Call answered by server for user ", callerId)
-    
-    const query = db.collection("sessions")
-  .orderBy("timestamp", "desc")
-  .limit(1);
-
-  console.log("i got the query", query)
-
-query
-  .get()
-  .then((querySnapshot) => {
-    console.log("Im in the query")
-    if (!querySnapshot.empty) {
-      const latestSession = querySnapshot.docs[0];
-      const sessionRef = db.collection("sessions").doc(latestSession.id);
-      
-      // Update the "isAnswered" field of the latest session
-      sessionRef
-        .update({
-          isAnswered: true, // Update other fields as needed
-        })
-        .then(() => {
-          console.log("Latest session updated successfully");
-        })
-        .catch((error) => {
-          console.error("Error updating latest session: ", error);
-        });
-    } else {
-      console.log("No matching sessions found for the provided userId.");
-    }
-  })
-  .catch((error) => {
-    console.error("Error getting sessions: ", error);
-  });
-    
-
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
-    });
-  });
+  // Existing socket events...
 
   socket.on("IceCandidate", (data) => {
     let calleeId = data.calleeId;
@@ -112,4 +43,70 @@ query
       iceCandidate: iceCandidate,
     });
   });
+});
+
+// CRUD operations for the "users" collection
+app.post("/users", async (req, res) => {
+  try {
+    const userData = req.body;
+    const newUserRef = await db.collection("users").add(userData);
+    res.status(201).json({ id: newUserRef.id });
+  } catch (error) {
+    console.error("Error creating user: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.put("/users/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    const newData = req.body;
+    await db.collection("users").doc(userId).update(newData);
+    res.status(200).send("User updated successfully");
+  } catch (error) {
+    console.error("Error updating user: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.delete("/users/:userId", async (req, res) => {
+  try {
+    const userId = req.params.userId;
+    await db.collection("users").doc(userId).delete();
+    res.status(200).send("User deleted successfully");
+  } catch (error) {
+    console.error("Error deleting user: ", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// User sign-in API
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Here, you should use Firebase Authentication for secure user sign-in
+    // Firebase Authentication handles password hashing and verification
+    // Example code to check email and password match (for demo purposes only):
+    
+    const userSnapshot = await db.collection("users")
+      .where("email", "==", email)
+      .where("password", "==", password)
+      .get();
+
+    if (!userSnapshot.empty) {
+      return res.status(200).send("Sign-in successful");
+    } else {
+      return res.status(401).send("Invalid credentials");
+    }
+  } catch (error) {
+    console.error("Error during sign-in: ", error);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
