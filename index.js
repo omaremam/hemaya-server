9cd8482d5048ec79fc870449825db71ceb256bf9
AbdelrahmanAshraf9079 <abdelrahman9709@gmail.com>
2023-10-07 03:31:47 +0300

    CORS Handling

--- index.js
+++ index.js
@@ -11,7 +11,15 @@ admin.initializeApp({
 const db = admin.firestore();
 const app = express();
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  bodyParser.json();
+  next();
+});
+
+// app.use(bodyParser.json());
 
 const port = process.env.PORT || 5000;
 const appPort = process.env.PORT || 5956;
